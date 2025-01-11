a78ad632e6f705092942728ba2b10dd45233e75d
Nunes6104 <150012302+Nunes6104@users.noreply.github.com>
2025-01-11 14:49:21 +0000

    Add files via upload

--- Lotaria/ViewController.swift
+++ Lotaria/ViewController.swift
@@ -9,17 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var btn1: UIButton!
-    
-    @IBOutlet weak var btn2: UIButton!
-    
-    @IBOutlet weak var btn3: UIButton!
-    
-    @IBOutlet weak var btn4: UIButton!
-    
-    @IBOutlet weak var btn5: UIButton!
-    
-    @IBOutlet weak var btn6: UIButton!
+
+    @IBOutlet var numeros: [UIButton]!
     
     
     
@@ -31,20 +22,11 @@ class ViewController: UIViewController {
 
     @IBAction func btn_Play(_ sender: UIButton) {
         //Genrate number when pressed
-        let number1 = Int.random(in: 1...50)
-        let number2 = Int.random(in: 1...50)
-        let number3 = Int.random(in: 1...50)
-        let number4 = Int.random(in: 1...50)
-        let number5 = Int.random(in: 1...50)
-        let number6 = Int.random(in: 1...50)
+        for numeros in numeros {
+            let randomNumber = Int.random(in: 1...50)
+            numeros.setTitle("\(randomNumber)", for: .normal)
+        }
         
-        //change the number in the btn screen
-        btn1.setTitle("\(number1)", for: .normal)
-        btn2.setTitle("\(number2)", for: .normal)
-        btn3.setTitle("\(number3)", for: .normal)
-        btn4.setTitle("\(number4)", for: .normal)
-        btn5.setTitle("\(number5)", for: .normal)
-        btn6.setTitle("\(number6)", for: .normal)
     }
     
 }
