dfaa10e6a21d2c1876df3bf45acd88625442a4ab
Nunes6104 <150012302+Nunes6104@users.noreply.github.com>
2025-01-11 13:38:37 +0000

    Add files via upload

---
+++ Lotaria/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Lotaria
+//
+//  Created by Aluno ISTEC on 11/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btn1: UIButton!
+    
+    @IBOutlet weak var btn2: UIButton!
+    
+    @IBOutlet weak var btn3: UIButton!
+    
+    @IBOutlet weak var btn4: UIButton!
+    
+    @IBOutlet weak var btn5: UIButton!
+    
+    @IBOutlet weak var btn6: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
