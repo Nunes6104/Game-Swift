59d8e5c525d3baf72fe8cbff6dac2cee876bf4cc
Nunes6104 <150012302+Nunes6104@users.noreply.github.com>
2025-01-11 14:10:35 +0000

    Add files via upload

--- Lotaria/ViewController.swift
+++ Lotaria/ViewController.swift
@@ -27,7 +27,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func btn_Play(_ sender: UIButton) {
+        //Genrate number when pressed
+        let number1 = Int.random(in: 1...50)
+        let number2 = Int.random(in: 1...50)
+        let number3 = Int.random(in: 1...50)
+        let number4 = Int.random(in: 1...50)
+        let number5 = Int.random(in: 1...50)
+        let number6 = Int.random(in: 1...50)
+        
+        //change the number in the btn screen
+        btn1.setTitle("\(number1)", for: .normal)
+        btn2.setTitle("\(number2)", for: .normal)
+        btn3.setTitle("\(number3)", for: .normal)
+        btn4.setTitle("\(number4)", for: .normal)
+        btn5.setTitle("\(number5)", for: .normal)
+        btn6.setTitle("\(number6)", for: .normal)
+    }
+    
 }
 
